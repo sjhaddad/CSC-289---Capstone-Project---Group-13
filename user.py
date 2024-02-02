bc30a853a0e10524b4ee6017b4a80029620f2c74
Allan_A <109562531+AllanA2k@users.noreply.github.com>
2024-02-02 17:00:07 -0500

    Update user.py
    added a "status" value for marital status choice which determines the tax amount discounted for the user

--- user.py
+++ user.py
@@ -1,12 +1,12 @@
 class User:
 
-    def __init__(self, first, last, total_income):
+    def __init__(self, first, last, status, total_income):
         self.first = first
         self.last = last
+        self.status = status
         self.total_income = total_income
         # https://www.ncdor.gov/taxes-forms/individual-income-tax/north-carolina-standard-deduction-or-north-carolina-itemized-deductions
         # standard deduction for single status is $12,750.
         # income tax rate is 4.75%
-        self.tax = round((total_income - 12750) * 0.0475, 2)
-
+        self.tax = round((total_income - status) * 0.0475, 2)
 
