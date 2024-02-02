bf591423bd17a2719ac7dad173fe49d3a154b84b
Allan_A <109562531+AllanA2k@users.noreply.github.com>
2024-02-02 16:58:24 -0500

    Update main.py
    Added the option to edit user income by name and display user information by name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from user import User
-from sqlite import create_table, add_user, display_table
+from sqlite import create_table, add_user, update_user, display_user_info_by_name, display_table
 
 def user_interface():
     print("\nWelcome to Shuttle Cash!")
@@ -20,18 +20,33 @@ while choice != 5:
         case 1:
             first_name = input("Enter first name: ")
             last_name = input("Enter last name: ")
+            marital_status = input("Single or married: ")
+            status = 0
+            if marital_status.capitalize() == "Single":
+                status = 12750
+            else:
+                status = 22500
             total_income = float(input("Enter total income: "))
-            if total_income <= 12750:   # If we don't include this, users with negative taxes owed would be stored in the database
+
+            if marital_status.capitalize() == "Single" and total_income <= 12750:
+                print("Your income is below the taxable amount")
+            if marital_status.capitalize() == "Married" and total_income <= 25500:   # If we don't include this, users with negative taxes owed would be stored in the database
                 print("Your income is below the taxable amount")
             else:
-                user1 = User(first_name, last_name, total_income)
+                user1 = User(first_name, last_name, status, total_income)
                 add_user(user1)
         case 2:
-            print("Placeholder :)")
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            total_income = float(input("Enter updated total income: "))
+            user_to_update = User(first_name, last_name, 0, total_income)
+            update_user(user_to_update)
         case 3:
             display_table()
         case 4:
-            print("Placeholder :)")
+            first_name = input("Enter first name: ")
+            last_name = input("Enter last name: ")
+            display_user_info_by_name(first_name, last_name)
         case 5:
             break
         case _:     # Default value in case user inputs a choice that isn't specified
