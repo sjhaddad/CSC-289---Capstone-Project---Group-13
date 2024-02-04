bb57632cc57e836b4f5bf319026cc857b79a9b4d
bryancramsey <bryancramsey@gmail.com>
2024-02-03 21:17:51 -0500

    added getters and setters for user

--- user.py
+++ user.py
@@ -10,3 +10,35 @@ class User:
         # income tax rate is 4.75%
         self.tax = round((total_income - status) * 0.0475, 2)
 
+    def get_first(self):
+        return self.first
+    
+    def get_last(self):
+        return self.last
+    
+    def get_status(self):
+        return self.status
+    
+    def get_total_income(self):
+        return self.total_income
+    
+    def get_tax(self):
+        return self.tax
+    
+    def set_first(self, first):
+        self.first = first
+    
+    def set_last(self, last):
+        self.last = last
+    
+    def set_status(self, status):
+        self.status = status
+    
+    def set_total_income(self, total):
+        self.total_income = total
+    
+    def set_tax(self, tax):
+        self.tax = tax
+    
+
+
