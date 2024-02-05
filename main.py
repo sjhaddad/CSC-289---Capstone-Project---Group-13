6082554c075152824170c08e3fd3460d467c25bc
Shawn-1992 <snpalmer@my.waketech.edu>
2024-02-05 10:31:38 -0500

    created separate function for every interface option

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from user import User
-from sqlite import create_table, add_user, update_user, display_user_info_by_name, display_table
+from interface_functions import *
+
 
 def user_interface():
     print("\nWelcome to Shuttle Cash!")
@@ -11,6 +11,7 @@ def user_interface():
     choice = input("Please enter your choice: ")
     return int(choice)
 
+
 create_table()
 
 choice = 0
@@ -18,37 +19,15 @@ while choice != 5:
     choice = user_interface()
     match choice:
         case 1:
-            first_name = input("Enter first name: ")
-            last_name = input("Enter last name: ")
-            marital_status = input("Single or married: ")
-            status = 0
-            if marital_status.capitalize() == "Single":
-                status = 12750
-            else:
-                status = 22500
-            total_income = float(input("Enter total income: "))
-
-            if marital_status.capitalize() == "Single" and total_income <= 12750:
-                print("Your income is below the taxable amount")
-            if marital_status.capitalize() == "Married" and total_income <= 25500:   # If we don't include this, users with negative taxes owed would be stored in the database
-                print("Your income is below the taxable amount")
-            else:
-                user1 = User(first_name, last_name, status, total_income)
-                add_user(user1)
+            new_user()
         case 2:
-            first_name = input("Enter first name: ")
-            last_name = input("Enter last name: ")
-            total_income = float(input("Enter updated total income: "))
-            user_to_update = User(first_name, last_name, 0, total_income)
-            update_user(user_to_update)
+            edit_income()
         case 3:
-            display_table()
+            display_info()
         case 4:
-            first_name = input("Enter first name: ")
-            last_name = input("Enter last name: ")
-            display_user_info_by_name(first_name, last_name)
+            display_by_name()
         case 5:
             break
-        case _:     # Default value in case user inputs a choice that isn't specified
+        case _:  # Default value in case user inputs a choice that isn't specified
             pass
 print("Thank you for using Shuttle Cash!")
